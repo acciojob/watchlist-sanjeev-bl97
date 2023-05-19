49bccac3b63354927cc3eafcece55a1a3918e036
sanjeev.bl97 <sanjeev.bl97@gmail.com>
2023-05-19 14:32:11 +0530

    Done

--- src/main/java/com/driver/Director.java
+++ src/main/java/com/driver/Director.java
@@ -1,15 +1,35 @@
 package com.driver;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class Director {
 
     private String name;
     private int numberOfMovies;
     private double imdbRating;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfMovies() {
+        return numberOfMovies;
+    }
+
+    public void setNumberOfMovies(int numberOfMovies) {
+        this.numberOfMovies = numberOfMovies;
+    }
+
+    public double getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(double imdbRating) {
+        this.imdbRating = imdbRating;
+    }
 
     public Director(String name, int numberOfMovies, double imdbRating) {
         this.name = name;
--- src/main/java/com/driver/Movie.java
+++ src/main/java/com/driver/Movie.java
@@ -1,15 +1,35 @@
 package com.driver;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class Movie {
 
     private String name;
     private int durationInMinutes;
     private double imdbRating;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    public double getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(double imdbRating) {
+        this.imdbRating = imdbRating;
+    }
 
     public Movie(String name, int durationInMinutes, double imdbRating) {
         this.name = name;
